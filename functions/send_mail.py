571ec3380893411097ac518aee0b677d1c2abd2a
feixiangingk <feixiangingk@163.com>
2017-05-03 00:56:08 +0800

    新增发送邮件类

--- functions/send_mail.py
+++ functions/send_mail.py
@@ -1,69 +1,96 @@
 #coding:utf-8
-import smtplib,os
+import yagmail,os
+from datetime import  datetime
+from appium_init import *
 from email.mime.text import MIMEText
+#上传附件需要的类
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
 class SendMail(object):
-	"""docstring for send_mail"""
+    """docstring for send_mail"""
+        #入参测试报告文件路径，查询文件夹，选取最新的文件，返回“文件路径+最新的文件名”
+    def get_report(self,result_path):
+        reportName=sorted(os.listdir(result_path),key=lambda filename:(os.path.getmtime(result_path+"\\"+filename)))[-1]
+        return (result_path+'\\'+reportName)
 
-	def get_report(self,result_path):
-		reportName=sorted(os.listdir(result_path),
-			key=lambda filename:(os.path.getmtime(result_path+"\\"+filename)))[-1]
-		return (result_path+'\\'+reportName)
+    def get_mail_body(self,report_file):
+        f = open(report_file, 'rb')
+        mail_body = f.read()
+        f.close()
+        return mail_body
 
-	def post_mail(self,report_file):
-		f=open(report_file,'rb')
-		mail_body=f.read()
-		f.close()
+    def post_mail(self,to_mail_list,resultFile,logFile):
 
+        mail = yagmail.SMTP(user='276476197@qq.com', password='gujttwszbatpbieh', host='smtp.qq.com', port='587')
+        contents = "UFO_appium test report"
+        mail.send(to=to_mail_list,subject=u'quark—UFO自动化测试报告',
+                  contents=[contents,resultFile,logFile])
 
-		sender="276476197@qq.com"
-		to_mail=["276476197@qq.com"]
-		username="276476197@qq.com"
+    def send(self):
+        to_mail_list=appium_init.inital.desired_caps['to_mail_list'].split(',')
+        project_path=appium_init.inital.project_path
 
-		#QQ授权码
-		mail_pwd="gujttwszbatpbieh"
-		# #第一种写法  使用plain格式发送
-		# msg=MIMEText("邮件测试",'plain','utf-8')
+        log_path=project_path+"\\log\\"+datetime.now().strftime("%Y_%m_%d")
+        result_path=project_path+ "\\result\\" +time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
+        logFile=self.get_report(log_path)
+        resultFile=self.get_report(result_path)
 
-		#第二种写法  使用html格式发送
-		# msg=MIMEText("<html><h1>邮件测试！</h1></html>","html","utf-8")
-		msg=MIMEText(mail_body,"html","utf-8")
-
-		msg['To'] =";".join(to_mail)
-		msg['From']='hello<'+username+'>'
-		msg['Subject']=Header(u'quark—UFO自动化测试报告','utf-8')
-		try:
-			smtp=smtplib.SMTP_SSL()
-			smtp.connect("smtp.qq.com")
-			smtp.login(username, mail_pwd)
-			smtp.sendmail(sender, to_mail, msg.as_string())
-			smtp.close()
-			return True
-		except Exception, e:
-			print str(e).encode('GBK')
-			return False
+        self.post_mail(to_mail_list,resultFile,logFile)
 
 
-# #邮件正文
-# msg=MIMEText("<html><h1>邮件测试！</h1></html>","html","utf-8")
-# msg["Subject"]=Header("python email test","utf-8")
+        # yag.send(to=['276476197@qq.com', 'FanGu@quarkfinance.com'], subject=u'quark—UFO自动化测试报告',
+        #          contents=[contents,
+        #                    "D:\\quarkscript\\UFO_appium\\result\\2017-05-01\\2017-05-01-07_54_03_result.html",
+        #                    "D:\\quarkscript\\UFO_appium\\log\\2017_05_01\\appium 2017_05_01 00-07-08.log"])
 
 
-# smtp=smtplib.SMTP()
-# smtp.connect("smtp.qq.com")
-# smtp.login(username, password)
-# smtp.sendmail(sender, to_addrs, msg.as_string())
 
-# smtp.quit()
+
+    # def post_mail(self,report_file):
+    #     msg=MIMEMultipart()
+    #     f=open(report_file,'rb')
+    #     mail_body=f.read()
+    #     f.close()
+    #     sender="276476197@qq.com"
+    #     to_mail=["276476197@qq.com"]
+    #     username="276476197@qq.com"
+    #
+    #     #QQ授权码
+    #     mail_pwd="gujttwszbatpbieh"
+    #
+    #     #实例化父类
+    #     msg=MIMEMultipart()
+    #     msg['To'] = ";".join(to_mail)
+    #     msg['From'] = 'hello<' + username + '>'
+    #     msg['Subject'] = Header(u'quark—UFO自动化测试报告', 'utf-8')
+    #
+    #     #创建正文部分并加载
+    #     text_part = MIMEText(mail_body, _subtype="html", _charset="utf-8")
+    #     msg.attach(text_part)
+    #     #
+    #     #创建附件部分并加载
+    #     upload_part=MIMEApplication(mail_body)
+    #     upload_part.add_header('Content-Disposition', 'attachment', filename="UFO_appium_report.html")
+    #     msg.attach(upload_part)
+    #
+    #     try:
+    #     	smtp=smtplib.SMTP_SSL()
+    #     	smtp.connect("smtp.qq.com")
+    #     	smtp.login(username, mail_pwd)
+    #     	smtp.sendmail(sender, to_mail, msg.as_string())
+    #     	smtp.close()
+    #     	return True
+    #     except Exception, e:
+    #     	print str(e).encode('GBK')
+    #     	return False
+
+
 
 
 if __name__ == '__main__':
-	mail=SendMail()
-	# if mail.post_mail():
-	# 	print "发送成功"
-	# else:
-	# 	print "发送失败"
-	report_file=mail.get_report("D:\\quarkscript\\UFO_appium\\result\\2017-04-27")
-	mail.post_mail(report_file)
+    Init()
+    mail=SendMail()
+    mail.send()
