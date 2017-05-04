d9f593f4d351730258492dd1f54f8e7eefa8bc90
LeHe <89605179@qq.com>
2017-05-04 14:41:16 +0800

    重新封装了GetHtml 方法，用于生成一个兼容邮件格式的测试报告

--- functions/exec_Suitecase.py
+++ functions/exec_Suitecase.py
@@ -3,6 +3,7 @@ from functions.HTMLTestRunner1 import  *
 # from functions.appium_init import Initialization
 from appium_init import *
 import os
+import GetHtml
 
 
 class exec_sutiecase():
@@ -20,73 +21,6 @@ class exec_sutiecase():
 
         # appium_init.inital.result_path=self.tdresult
 
-    def get_html(self,stream,startTime, duration, status, success_count, failure_count, error_count):
-        """
-        :param startTime: 开始时间
-        :param duration: 运行时长
-        :param status: 状态
-        :param success_count: 通过数量
-        :param failure_count: 失败数量
-        :param error_count：错误数量
-        :return:
-        """
-
-        template = '''
-        <h1 style="color:green;">quark—UFO自动化测试报告</h1>
-        开始时间:{startTime}<br/>
-        运行时长:{duration}<br/>
-        状态:{status}<br/>
-        <p style="color:blue;">结果概览 </p>
-              <h2>详细数据，烦请点击查看附件</h2>
-              <div align=center>
-        <table border=1 cellspacing=0 cellpadding=0 style='border-collapse:collapse;border:none'>
-            <tr>
-                <td   width=180 valign=top style='width:134.7pt;border:solid windowtext 1.0pt;background:#DBDBDB;padding:0cm 5.4pt 0cm 5.4pt'>
-                <p class=MsoNormal align=center style='text-align:center'>
-                    <span style='font-family:宋体'>用例总数</span><span lang=EN-US><o:p></o:p></span></p>
-                    </td>
-                <td   width=180 valign=top style='width:134.7pt;border:solid windowtext 1.0pt;background:#DBDBDB;padding:0cm 5.4pt 0cm 5.4pt'>
-                <p class=MsoNormal align=center style='text-align:center'>
-                    <span style='font-family:宋体'>通过数量</span><span lang=EN-US><o:p></o:p></span></p>
-                    </td>
-                 <td   width=180 valign=top style='width:134.7pt;border:solid windowtext 1.0pt;background:#DBDBDB;padding:0cm 5.4pt 0cm 5.4pt'>
-                <p class=MsoNormal align=center style='text-align:center'>
-                    <span style='font-family:宋体'>失败数量</span><span lang=EN-US><o:p></o:p></span></p>
-                </td>
-                <td   width=180 valign=top style='width:134.7pt;border:solid windowtext 1.0pt;background:#DBDBDB;padding:0cm 5.4pt 0cm 5.4pt'>
-                <p class=MsoNormal align=center style='text-align:center'>
-                    <span style='font-family:宋体'>错误数量</span><span lang=EN-US><o:p></o:p></span></p>
-                    </td>
-            </tr>
-
-            <tr>
-               <td   width=180 valign=top style='width:134.7pt;border:solid windowtext 1.0pt;padding:0cm 5.4pt 0cm 5.4pt'>
-                <p class=MsoNormal align=center style='text-align:center'>
-                    <span style='font-family:宋体'>{full_success}</span><span lang=EN-US><o:p></o:p></span></p>
-                    </td>
-                <td   width=180 valign=top style='width:134.7pt;border:solid windowtext 1.0pt;padding:0cm 5.4pt 0cm 5.4pt'>
-                <p class=MsoNormal align=center style='text-align:center'>
-                    <span style='font-family:宋体'>{success_count}</span><span lang=EN-US><o:p></o:p></span></p>
-                    </td>
-                 <td  bgcolor="red"   width=180 valign=top style='width:134.7pt;border:solid windowtext 1.0p;padding:0cm 5.4pt 0cm 5.4pt'>
-                <p class=MsoNormal align=center style='text-align:center'>
-                    <span style='font-family:宋体'>{failure_count}</span><span lang=EN-US><o:p></o:p></span></p>
-                </td>
-                <td   width=180 valign=top style='width:134.7pt;border:solid windowtext 1.0pt;background:0cm 5.4pt 0cm 5.4pt'>
-                <p class=MsoNormal align=center style='text-align:center'>
-                    <span style='font-family:宋体'>{error_count}</span><span lang=EN-US><o:p></o:p></span></p>
-                    </td>
-            </tr>
-        </table>
-        </div>
-        '''.format(startTime=startTime, duration=duration, status=status,
-                   full_success=success_count + failure_count + error_count, success_count=success_count,
-                   failure_count=failure_count, error_count=error_count)
-        stream.write(template)
-
-        # save_html_file = '%s/test.html' % result_path
-
-
 
 
     def exec_cases(self,test_case):
@@ -102,12 +36,12 @@ class exec_sutiecase():
             runner = HTMLTestRunner(stream=fp, title=u'quark—UFO自动化测试报告', description=u'用例详情：',verbosity=2)
 
 
-
-
             # 运行测试用例
             a=runner.run(test_case)
+            startTime = datetime.datetime.now()
+            startTime = str(startTime)[:19]
 
-            self.get_html(fp1,  a.startTime,a.duration,a.status, a.success_count, a.failure_count, a.error_count)
+            GetHtml.get_html(fp1, startTime, a.success_count, a.failure_count, a.error_count)
             fp.close()  # 关闭报告文件
             fp1.close()
 
@@ -123,6 +57,8 @@ class exec_sutiecase():
             # 运行测试用例
             a=runner.run(test_case)
 
-            self.get_html(fp1, a.startTime,a.duration,a.status, a.success_count, a.failure_count, a.error_count)
+            startTime = datetime.datetime.now()
+            startTime = str(startTime)[:19]
+            GetHtml.get_html(fp1, startTime,a.success_count, a.failure_count, a.error_count)
             fp.close()  # 关闭报告文件
             fp1.close()
