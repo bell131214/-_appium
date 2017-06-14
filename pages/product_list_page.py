a80efc88aea5d2b008fbed59fd7eb3b9e99d6473
LeHe <89605179@qq.com>
2017-06-14 09:37:01 +0800

    资产详情新增测试用例4条，新增入口page页面

--- pages/product_list_page.py
+++ pages/product_list_page.py
@@ -3,6 +3,7 @@ from functions.BasePage import BasePage
 from functions.appium_init import *
 from selenium.webdriver.common.by import By
 from pages.product_quarkzx_page import ProductQuarkzxPage
+from pages.my_invset_record_page import MyInvsetRecorePage
 
 class ProductListPage(BasePage):
 
@@ -13,7 +14,18 @@ class ProductListPage(BasePage):
     def el_quarkZX_btn(self):
         return self.base_find_element(By.XPATH,"//android.widget.TextView[contains(@text,'夸客尊享')]")
 
+    #投资记录悬浮层
+    @property
+    def el_financia_img(self):
+        return self.base_find_element(By.ID,"com.quarkfinance.ufo:id/return_money_btn")
+
+
+
     #点击夸客尊享链接，跳转至夸客尊享产品详情page
     def logic_link_quarkZX(self):
         self.el_quarkZX_btn.click()
         return ProductQuarkzxPage(self.driver)
+
+    def logic_financia_img_click(self):
+        self.el_financia_img.click()
+        return  MyInvsetRecorePage(self.driver)
